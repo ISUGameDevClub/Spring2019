902695d3620774d46745267f03923867d8c58209
Niko Rizo <49258126+Niko-Rizo@users.noreply.github.com>
2019-04-10 21:09:33 -0500

    Merger of files for Niko

---
+++ Assets/Scripts/TriggerBattle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerBattle : MonoBehaviour
+{
+
+    public Animator animator;
+
+    
+    private Animation Fade_Out;
+
+    private int levelToLoad;
+
+    public string sceneToLoad;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            Destroy(GameObject.Find("Enemy"));
+            Fade_Out.Play();
+            FadeToLevel(1);
+            Application.LoadLevel(sceneToLoad);
+            //LoadBattleScreen();
+        }
+    }
+    
+    void LoadBattleScreen()
+    {
+        GameController.control.inBattle = true;
+        Destroy(GameObject.Find("Enemy"));
+        GameController.control.playerDead = true;
+        Application.LoadLevel(sceneToLoad);
+    }
+
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+        //Application.LoadLevel(sceneToLoad);
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+        OnFadeComplete();
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
