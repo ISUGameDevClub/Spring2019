43853c63f492eee0b7586f6b07396ccf4e4ecb7d
Brown, Parker <parker.brown1305@gmail.com>
2019-04-10 21:31:00 -0500

    added quest.cs with data of quest, questgiver.cs to hand out quests, and modified player to trigger quests

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,24 +8,42 @@ public class Player : MonoBehaviour
     public int maxHealth = 10;
     public int currentHealth = 5;
 
-    //--------------------------Brackey's tutorial methods. Saving and Loading should be done in game manager script/object(?)----------------------
-    /*
-    public void SavePlayer()
-    {
-        SaveSystem.SavePlayer(this);
-    }
-    public void LoadPlayer()
-    {
-        PlayerData data = SaveSystem.LoadPlayer();
+    public List<Quest> activeQuests;
+    public int exp;
+    public int gold;
+    public int interactRange;
+    public LayerMask whatIsQuestgiver;
 
-        level = data.level;
-        maxHealth = data.maxHealth;
-        currentHealth = data.currentHealth;
-
-        Vector2 position;
-        position.x = data.position[0];
-        position.y = data.position[1];
-        transform.position = position;
+    void FixedUpdate()
+    {
+        interact();
     }
-    */
+
+    void Start()
+    {
+        activeQuests = new List<Quest>();
+    }
+    
+    //uses E as keybind
+    public void interact()
+    {
+        if (Input.GetAxis("Interact") == 1)
+        {
+            //check for any questgivers, if any, show quest window
+            Collider2D target = Physics2D.OverlapCircle(gameObject.transform.position, interactRange, whatIsQuestgiver);
+            if (target != null)
+            {
+                target.gameObject.GetComponent<Questgiver>().OpenQuestWindow();
+            }
+
+            //other interactions can go here too
+
+        }
+    }
+
+    //debug purposes
+    public void PrintQuests()
+    {
+        Debug.Log("Quest: " + activeQuests[0].title);
+    }
 }
---
+++ Assets/Scripts/Questgiver.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * As of now, each questgiver can only give 1 quest, simpler to implement
+ * 
+ * 
+ */
+public class Questgiver : MonoBehaviour
+{
+    public Quest quest;
+    public Player player;
+
+    public GameObject questWindow;
+    public Text titleText;
+    public Text descriptionText;
+    public Text experienceText;
+    public Text goldText;
+
+    public int questWindowViewDistance;
+    public LayerMask whatIsPlayer;
+
+    //no drag and drop for each questgiver
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        questWindow = GameObject.Find("QuestWindow");
+    }
+
+    void FixedUpdate()
+    {
+        ProximityClose();
+    }
+
+    public void OpenQuestWindow()
+    {
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+        experienceText.text = quest.expReward.ToString();
+        goldText.text = quest.goldReward.ToString();
+    }
+
+    public void CloseQuestWindow()
+    {
+        questWindow.SetActive(false);
+    }
+
+    //closes quest window if player exits specified range
+    public void ProximityClose()
+    {
+        //check for any questgivers, if none exist, close the active window
+        Collider2D target = Physics2D.OverlapCircle(gameObject.transform.position, questWindowViewDistance, whatIsPlayer);
+        if (target == null && gameObject.GetComponent<Questgiver>().questWindow.activeInHierarchy)
+        {
+            cout 
+            CloseQuestWindow();
+        }
+    }
+
+    public void AcceptQuest()
+    {
+        questWindow.SetActive(false);
+        quest.isActive = true;
+        player.activeQuests.Add(quest);
+        player.GetComponent<Player>().PrintQuests();
+    }
+
+}
